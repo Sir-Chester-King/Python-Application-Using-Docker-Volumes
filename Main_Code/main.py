7899d72ff8f1bc7d8721a199e20331a4aa66b8b3
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-08-22 16:18:23 +0200

    Update

--- Main_Code/main.py
+++ Main_Code/main.py
@@ -1,12 +1,50 @@
 # Main function of program.
 # The purpose of this application is to create objects "user" and store their info's in a file.
+import os
+
+import Create_Users.Create_users
+import Modify_Users.Modify_users
+import View_Users.View_users
+
+
+def clean_console():
+    # For Windows environment
+    if os.name == 'nt':
+        os.system('cls')
+    # For Linux/Unix environment
+    else:
+        os.system("clear")
+
+
 def main():
-    print("Hello user !!!", end="\n")
-    print("You can create new User or you can view what users are stored in the file.", end="\n")
-    print("What you wanna choose?", end="\n")
+    clean_console()
+    menu_app = {
+        "1": "Create new user",
+        "2": "View list users",
+        "3": "Modify user's info"
+    }
+    options_available = list(menu_app.keys())
 
+    # Print the menu app.
+    print("{:<10} {:<15}".format('Option', 'Action'))
+    for key, value in menu_app.items():
+        print("{:<10} {:<15}".format(key, value))
+    print("Please, insert only the available value from the menu.")
 
+    option_chosen = str(input("Insert option: "))
+    while option_chosen not in options_available:
+        option_chosen = str(input("Insert option: "))
 
+    # Call the property function based on the user's chosen option.
+    match option_chosen:
+        case "1":
+            Create_Users.Create_users.new_user()
+        case "2":
+            View_Users.View_users.list_users()
+        case "3":
+            Modify_Users.Modify_users.modify_user()
+        case _:
+            return 0
 
 
 # Using the special variable __name__
--- Store_Data/Store_Data.py
+++ Store_Data/Store_Data.py
@@ -1,4 +1,4 @@
 # This function store the personal user's data in a file.
 # The path of the file will be inside the project's directory.
-def Store_Data_In_A_File():
-    pass
+def Store_Data_In_A_File(option_chosen):
+    print("SSS", option_chosen)
