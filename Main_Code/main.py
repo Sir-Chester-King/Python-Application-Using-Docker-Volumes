9b35c3ed2a518f909e59155ee8bc41ded5e533c9
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-08-22 12:15:41 +0200

    Update main.py

--- Main_Code/main.py
+++ Main_Code/main.py
@@ -1,7 +1,12 @@
 # Main function of program.
 # The purpose of this application is to create objects "user" and store their info's in a file.
 def main():
-    pass
+    print("Hello user !!!", end="\n")
+    print("You can create new User or you can view what users are stored in the file.", end="\n")
+    print("What you wanna choose?", end="\n")
+
+
+
 
 
 # Using the special variable __name__
